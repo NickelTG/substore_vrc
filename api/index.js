7dd71c13240d022bdb3fce196835b52e818f8c0d
Ev <emp1860@gmail.com>
2026-01-19 11:13:15 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
-const { spawn } = require('child_process');
+const { fork } = require('child_process');
 const path = require('path');
 
 module.exports = (req, res) => {
-  // Это запуск бинарного файла Sub-Store, который мы скачали при сборке
-  const subStore = spawn(path.join(__dirname, '..', 'sub-store'), ['-p', '3001']);
+  // Указываем путь к бандлу
+  const scriptPath = path.join(process.cwd(), 'sub-store.bundle.js');
 
-  subStore.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
+  // Параметры запуска для Node.js
+  const child = fork(scriptPath, ['--port', '3001'], {
+    env: { 
+      ...process.env, 
+      SUB_STORE_FRONTEND_BACKEND_PATH: process.env.SUB_STORE_FRONTEND_BACKEND_PATH 
+    }
   });
 
-  // Перенаправляем запрос на запущенный процесс
-  res.status(200).send("Backend is starting... Try /sub-store/ in a moment.");
+  // Отправляем ответ, чтобы Vercel не убил функцию мгновенно
+  res.status(200).send("Sub-Store Bundle is active. Connect via https://sub-store.vercel.app/");
 };
